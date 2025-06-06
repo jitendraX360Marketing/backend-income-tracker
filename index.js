be7f431178bc93da71a44174852c93d7a72fe15c
jitendraX360Marketing <officialjangidjitendra@gmail.com>
2025-06-06 23:24:12 +0530

    final push with cors

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const authRouter = require('./routes/auth');
+const authRouter = require("./routes/auth");
 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
 app.use(express.json());
 
-app.use('/api', authRouter);
+app.use("/api", authRouter);
 
 // Simple GET route
 app.get("/", (req, res) => {
